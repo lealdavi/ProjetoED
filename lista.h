3875c7869e87bf5ab7d6538fd22fa46ab755d3ff
Davi Leal <davileal1401@outlook.com>
2025-02-27 15:42:00 -0300

    implement lista using void pointers to support ADT operations

--- lista.h
+++ lista.h
@@ -1,29 +1,31 @@
 #ifndef LISTA_H
 #define LISTA_H
 
-typedef int T;
+typedef void *T;
 
-typedef struct node {
-    T dado;                          
+typedef struct node
+{
+    T dado;
     struct node *proximo, *anterior;
 } Node;
 
-typedef struct {
+typedef struct
+{
     Node *sentinela;
     int qntd, max;
 } Lista;
 
-Lista* inicializar (int max);
-int vazia (Lista *l);
-int cheia (Lista *l);
-void destruir (Lista *l);
-int removerInicio (Lista *l);
-int removerFim (Lista *l);
-int inserirInicio (Lista *l, T dado);
-int inserirFim (Lista *l, T dado);
-T primeiroElemento (Lista *l);
-T ultimoElemento (Lista *l);
-int quantidade (Lista *l);
-void imprimirLista (Lista *l);
+Lista *lista_inicializar(int max);
+int lista_vazia(Lista *l);
+int lista_cheia(Lista *l);
+void lista_destruir(Lista *l);
+int removerInicio(Lista *l);
+int removerFim(Lista *l);
+int inserirInicio(Lista *l, T dado);
+int inserirFim(Lista *l, T dado);
+T lista_primeiroElemento(Lista *l);
+T lista_ultimoElemento(Lista *l);
+int lista_quantidade(Lista *l);
+void imprimirLista(Lista *l, void (*print)(T));
 
 #endif
